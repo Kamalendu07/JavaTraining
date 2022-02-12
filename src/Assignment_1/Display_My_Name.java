bee2c2497f9ba6cfc125827729c5b3ff30ad5958
Kamalendu07 <98376043+Kamalendu07@users.noreply.github.com>
2022-02-12 21:44:37 +0530

    Add Training Code

---
+++ src/Assignment_1/Display_My_Name.java
@@ -0,0 +1,18 @@
+package Assignment_1;
+
+class Display_My_Name {
+
+	void display() {
+		System.out.println("I Am Kamalendu");
+	}
+}
+
+class My_Name {
+
+	public static void main(String[] args) {
+
+		Display_My_Name obj = new Display_My_Name();
+		obj.display();
+	}
+
+}
---
+++ src/Assignment_3/ProductNumber.java
@@ -0,0 +1,11 @@
+package Assignment_3;
+
+public class ProductNumber {
+	int a = 4, b = 5, c = 6, p = 1;
+
+	public static void main(String[] args) {
+		int a = 4, b = 5, c = 6, p = 1;
+		p = a * b * c;
+		System.out.println(p);
+	}
+}
