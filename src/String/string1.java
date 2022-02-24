9dedab5ed534ec67880098f68a62507b07893a0c
Kamalendu07 <98376043+Kamalendu07@users.noreply.github.com>
2022-02-24 19:56:28 +0530

    Create string1.java

---
+++ src/String/string1.java
@@ -0,0 +1,9 @@
+public class string1 {
+public static void main (String[] args) {
+ 	
+	String name = "Kamalendu";
+	System.out.println(name);
+	int value = name.length();
+	System.out.println(value);
+	}
+}
