e3f4df8dd39ae9b1f9d5bf696e10b01131d17cf4
Kamalendu07 <98376043+Kamalendu07@users.noreply.github.com>
2022-02-24 20:36:16 +0530

    Create B.java

---
+++ src/Constractor/AB/B.java
@@ -0,0 +1,23 @@
+
+	public class B extends A {
+
+		public static void main(String[] args) {
+			
+			B obj=new B();
+			obj.demob();
+			
+		}
+		public void demob()
+		{
+			System.out.println("For the Greater of 3 Numbers:");
+			int[] arr1=accept();
+			System.out.println("The value of size is ="+size);
+			for(i=0;i<size;i++)
+			{
+				System.out.println("The arr="+arr1[i]);
+			}
+			System.out.println("Do the process of grt of 3 no");
+		}
+		
+
+	}
