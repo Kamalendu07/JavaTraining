e714348fcdec0d2deeeaacf80767396ee9351293
Kamalendu07 <98376043+Kamalendu07@users.noreply.github.com>
2022-02-24 19:56:21 +0530

    Delete string1.java

--- src/string1.java
+++
@@ -1,9 +0,0 @@
-public class string1 {
-public static void main (String[] args) {
- 	
-	String name = "Kamalendu";
-	System.out.println(name);
-	int value = name.length();
-	System.out.println(value);
-	}
-}
