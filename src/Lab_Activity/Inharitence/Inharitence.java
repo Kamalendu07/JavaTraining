7339865a48a52b4dda72180546d6a952eb5992c5
Kamalendu07 <98376043+Kamalendu07@users.noreply.github.com>
2022-04-07 19:55:00 +0530

    Create Inharitence.java

---
+++ src/Lab_Activity/Inharitence/Inharitence.java
@@ -0,0 +1,15 @@
+
+public class Inharitence {
+	public static void main(String[] args) {
+
+		Manager m = new Manager(1, "peter", "chennai India", 237844, 65000);
+		Trainee t = new Trainee(2, "jack", "maumabi India", 442085, 45000);
+
+		m.calculateSalary();
+		t.calculateSalary();
+
+		m.calculateTransportAllowance();
+		t.calculateTransportAllowance();
+
+	}
+}
